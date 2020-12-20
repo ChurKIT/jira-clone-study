91c0146b452ca33ea816f30ca9d310f9d9c5e15e
ChurKIT <aleksey.churckin@yandex.ru>
2020-12-20 22:47:55 +0400

    fix bugs + add javafx function + add auth window

---
+++ src/main/java/edu/jiraclone/Controller/Controller.java
@@ -0,0 +1,38 @@
+package edu.jiraclone.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button authSingInButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button authSingUpButton;
+
+    @FXML
+    private TextField loginField;
+
+
+
+    @FXML
+    void initialize() {
+
+
+    }
+}
+
