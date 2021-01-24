de2f88add5a25159f45c9c036f0cface9aa7ef12
ChurKIT <aleksey.churckin@yandex.ru>
2021-01-24 21:19:13 +0400

    add singIn/singUp functions + mainPage

--- src/main/java/edu/jiraclone/Controller/SingInController.java
+++ src/main/java/edu/jiraclone/Controller/SingInController.java
@@ -4,16 +4,20 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import edu.jiraclone.Users.User;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class SingInController {
 
@@ -35,26 +39,71 @@ public class SingInController {
     @FXML
     private TextField authLoginField;
 
+    @FXML
+    private void singUpPage(){
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/singUp.fxml"));
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(new Scene(root));
+            stage.show();
+            authSingUpButton.getScene().getWindow().hide();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void toMainPage(){
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/mainWindow.fxml"));
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(new Scene(root));
+            stage.show();
+            authSingUpButton.getScene().getWindow().hide();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void incorrectLoginOrPass(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+
+        alert.setTitle("Incorrect login or password");
+        alert.setHeaderText(null);
+        alert.setContentText("Incorrect login or password. Repeat or SingUp");
+
+        alert.showAndWait();
+    }
+
 
 
 
     @FXML
     void initialize() {
-        authSingUpButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                authSingUpButton.getScene().getWindow().hide();
-                Parent root = null;
-                try {
-                    root = FXMLLoader.load(getClass().getResource("/singUp.fxml"));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                Scene scene = new Scene(root);
-                Stage stage = new Stage();
-                stage.setScene(scene);
-                stage.showAndWait();
+
+        authSingUpButton.setOnAction(event -> {
+            singUpPage();
+        });
+
+
+        authSingInButton.disableProperty().bind(
+                authLoginField.textProperty().isEmpty()
+                    .or(authPasswordField.textProperty().isEmpty())
+        );
+
+        authSingInButton.setOnAction(event -> {
+            User.singIn(authLoginField.getText(), authPasswordField.getText());
+            if(User.isLogIn()){
+                toMainPage();
+            } else {
+                incorrectLoginOrPass();
             }
+
         });
 
     }
--- src/main/java/edu/jiraclone/Controller/SingUpController.java
+++ src/main/java/edu/jiraclone/Controller/SingUpController.java
@@ -1,13 +1,22 @@
 package edu.jiraclone.Controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import edu.jiraclone.Users.Roles;
+import edu.jiraclone.Users.User;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class SingUpController {
 
@@ -33,7 +42,7 @@ public class SingUpController {
     private CheckBox regCheckAgree;
 
     @FXML
-    private ChoiceBox<?> regRoleChoiseBox;
+    private ChoiceBox<Roles> regRoleChoiceBox;
 
     @FXML
     private PasswordField regPasswordField;
@@ -41,11 +50,58 @@ public class SingUpController {
     @FXML
     private Button regSingUpButton;
 
+    @FXML
+    private void configRegRoleChoiceBox(){
+        ObservableList<Roles> roles = FXCollections.observableArrayList(Roles.values());
+        regRoleChoiceBox.setItems(roles);
+        regRoleChoiceBox.setValue(roles.get(3));
+    }
+
+    @FXML
+    private void returnToSingInPage(){
+        Parent root1 = null;
+        try {
+            root1 = FXMLLoader.load(getClass().getResource("/singIn.fxml"));
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(new Scene(root1));
+            stage.show();
+            regBackToSingInPage.getScene().getWindow().hide();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
     @FXML
     void initialize() {
+        configRegRoleChoiceBox();
+
+        regSingUpButton.disableProperty().bind(
+                regLoginField.textProperty().isEmpty()
+                .or(regPasswordField.textProperty().isEmpty())
+                .or(regNameField.textProperty().isEmpty())
+                .or(regSurnameField.textProperty().isEmpty())
+                .or(regCheckAgree.selectedProperty())
+        );
+
+
+
+        regSingUpButton.setOnAction(event -> {
+            //Создание нового юзера
+            new User(regLoginField.getText(), regPasswordField.getText(), regNameField.getText(), regSurnameField.getText(), regRoleChoiceBox.getValue());
+            returnToSingInPage();
+        });
+
+        regBackToSingInPage.setOnAction(event -> {
+            returnToSingInPage();
+        });
+
+
+
 
 
     }
+
 }
